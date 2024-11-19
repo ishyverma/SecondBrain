418249dd65cdd23b905f30b7466a58d1e384f377
ishyverma <ishyamver@gmail.com>
2024-11-19 23:48:32 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
-import mongoose from "mongoose"
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 const app = express()
 
@@ -24,3 +23,10 @@ app.delete("/api/v1/content", (req, res) => {
 
 })
 
+app.post("/api/v1/brain/share", (req, res) => {
+
+})
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {
+
+})
